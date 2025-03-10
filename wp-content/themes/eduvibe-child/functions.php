16680b03a712620dc6e60dbcb194c6185fdff18d
dharmik <pateldharmik1150@gmail.com>
2025-03-10 09:19:59 +0530

    Initial commit

--- wp-content/themes/eduvibe-child/functions.php
+++ wp-content/themes/eduvibe-child/functions.php
@@ -5,3 +5,26 @@ function eduvibe_child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'eduvibe_child_enqueue_styles', 100 );
+
+function replace_student_text_script() {
+    ?>
+    <script>
+    jQuery(document).ready(function($) {
+        function replaceStudentText() {
+            $('body').html(function(i, html) {
+                return html.replace(/Students/g, 'people');
+            });
+        }
+
+        // Call the function on page load
+        replaceStudentText();
+
+        // Optional: Call the function again when AJAX content loads
+        $(document).on('ajaxComplete', function() {
+            replaceStudentText();
+        });
+    });
+    </script>
+    <?php
+}
+add_action('wp_footer', 'replace_student_text_script');
